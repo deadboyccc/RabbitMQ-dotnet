d73ebd3032477b52fb916a06770940e321674fd1
Ahmed Ali <ahmed.mohammed.ali9999@gmail.com>
2025-01-31 20:24:26 +0300

    debug files

--- producer/obj/Debug/net9.0/producer.AssemblyInfo.cs
+++ producer/obj/Debug/net9.0/producer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("producer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+099613ec4a39ff7936e4f913aa00a4327d70586b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f37608d366458bf70921ed31a40507e6d4aeec5b")]
 [assembly: System.Reflection.AssemblyProductAttribute("producer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("producer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
