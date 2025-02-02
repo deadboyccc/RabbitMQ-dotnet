b2471cd10d7248a136b2824206f4eff71ae03f34
Ahmed Ali <ahmed.mohammed.ali9999@gmail.com>
2025-02-02 21:41:04 +0300

    started the Request Response Pattern implementation (kinda gPRC)

---
+++ RequestReplayPattern/client/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+
+internal class RabbitMQSender
+{
+
+  public static async Task Main(string[] args)
+  {
+    // creating the connection factory to create our initial conneciton
+    var factory = new ConnectionFactory { HostName = "localhost" };
+
+    // creating a single connnection 
+    using var connection = factory.CreateConnectionAsync();
+
+    // creating a channel or multiple ones depending on the need
+    using var channel = connection.Result.CreateChannelAsync();
+
+    // Creating an exchange (fanout send to all binded queues)
+    await channel.Result.ExchangeDeclareAsync(exchange: "pubsub", type: ExchangeType.Fanout);
+
+    // creating a queue
+    // await channel.Result.QueueDeclareAsync(queue: "first_queue", durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+    // Message id 
+    uint messageId = 0;
+    while (true)
+    {
+      var processingTime = new Random().Next(1, 3);
+      // creating the message
+      var message = $"{++messageId}: test msg from .net & took {processingTime}s to send";
+
+      // encoding the message to bytes (stream of binary to stream/send)
+      var encodedMsgBody = Encoding.UTF8.GetBytes(message);
+
+      // we have to public to an exchange - "" = default exchange 
+      await channel.Result.BasicPublishAsync(exchange: "pubsub", routingKey: "", body: encodedMsgBody);
+
+      // confirm by logging to console
+      Console.WriteLine($" [x] Sent {message} & took {processingTime}s to send");
+
+      // simulate processing the message 
+      await Task.Delay(TimeSpan.FromSeconds(processingTime));
+
+    }
+  }
+}
--- RequestReplayPattern/server/Program.cs
+++ RequestReplayPattern/server/Program.cs
@@ -16,17 +16,24 @@ internal class RabbitMQConsumer
 
     //we declare the exchange here to incase the consumer starts first, it will initiate the channel
     // if channel already exists == no problem 
-    await channel.Result.ExchangeDeclareAsync(exchange: "DirectRouting", ExchangeType.Direct);
+    await channel.Result.ExchangeDeclareAsync(exchange: "pubsub", type: ExchangeType.Fanout);
 
     //declare temp queue to consume from
     var queueName = await channel.Result.QueueDeclareAsync();
 
+    // Bind the queue to the exchange
+    await channel.Result.QueueBindAsync(queue: queueName, exchange: "pubsub", routingKey: "");
 
 
+
+    // Declare Named* queue to consume from
+    // await channel.Result.QueueDeclareAsync(queue: "first_queue", durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+    // setting channel settings to test competing consumer pattern 
+    // prefetchsize = max size to fetch, 0 == no limit
+    // prefetchCount = 1 , meaning 1 msg at a time
+    // global=false, this qos settings apply only for this consumer
     await channel.Result.BasicQosAsync(prefetchSize: 0, prefetchCount: 1, global: false);
-
-    //binding the channel 
-    await channel.Result.QueueBindAsync(queueName, exchange: "DirectRouting", routingKey: "payments");
 
     // Create consumer 
     var consumer = new AsyncEventingBasicConsumer(channel.Result);
@@ -41,8 +48,10 @@ internal class RabbitMQConsumer
       // Simulate processing the message
       var body = msg.Body.ToArray();
       var message = Encoding.UTF8.GetString(body);
-      Console.WriteLine($" [Payments] Received {message} | Took {processingTime}s to recieve");
+      Console.WriteLine($" [x] Received {message} | Took {processingTime}s to recieve");
 
+      // Simulating waiting time to examine the competing consumers pattern
+      // between 1-4 seconds
       await Task.Delay(TimeSpan.FromSeconds(processingTime)); // Simulate waiting time
       // manual ack
       await channel.Result.BasicAckAsync(deliveryTag: msg.DeliveryTag, multiple: false);
@@ -51,7 +60,7 @@ internal class RabbitMQConsumer
     // Start Readind/Consuming messages from the queue 
     await channel.Result.BasicConsumeAsync(queue: queueName, autoAck: false, consumer: consumer);
 
-    Console.WriteLine(" [Payments] Waiting for messages. Press [enter] to exit.");
+    Console.WriteLine(" [*] Waiting for messages. Press [enter] to exit.");
     Console.ReadLine(); // Keep the program running
   }
 }
